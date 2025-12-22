44548d093c4e03bcf0ceebab5279d92887dd81e3
squar3xx <horw1xx11@gmail.com>
2025-12-23 01:29:15 +0300

    Add static files support

--- source/server.c
+++ source/server.c
@@ -10,6 +10,66 @@
 #define PORT 8080 
 #define BACKLOG 10 
 
+#define WEB_DIR "www"
+
+void send_file(int client_fd, const char* filepath) {
+  
+  FILE* file = fopen(filepath, "rb");
+  if (!file) {
+    char* error = 
+      "HTTP/1.1 404 Not Found\r\n"
+      "Content-Type: text/html\r\n"
+      "Content-Length: 20"
+      "\r\n"
+      "404 - File Not Found";
+    send(client_fd, error, strlen(error), 0);
+    return;
+  }
+
+  fseek(file, 0, SEEK_END);
+  long file_len = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  
+  const char* mime_type = "text/html";
+  const char* ext = strrchr(filepath, '.');
+
+  if (ext != NULL) {
+    if (strcmp(ext, ".html") == 0 || strcmp(ext, ".htm") == 0) {
+      mime_type = "text/html";
+    } else if(strcmp(ext, ".css") == 0) {
+      mime_type = "text/css";
+    } else if(!strcmp(ext, ".js")) {
+      mime_type = "application/javascript";
+    } else if(strcmp(ext, ".png") == 0) {
+      mime_type = "image/png";
+    } else if(strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0) {
+      mime_type = "image/jpg";
+    } else if(strcmp(ext, ".gif") == 0) {
+      mime_type = "image/gif";
+    } else if (strcmp(ext, ".ico") == 0) {
+      mime_type = "image/x-icon";
+    }
+  }
+
+  char headers[512];
+  int header_length = snprintf(headers, sizeof(headers), 
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: %s\r\n"
+    "Content-Length: %ld\r\n"
+    "\r\n"
+  , mime_type, file_len);
+  send(client_fd, headers, header_length, 0);
+
+  char buffer[4096];
+  size_t bytes_read;
+  while ((bytes_read = fread(buffer, sizeof(char), sizeof(buffer), file)) > 0) {
+    write(client_fd, buffer, bytes_read);
+  }
+
+  fclose(file);
+  printf("Файл отправлен");
+}
+
 int main () {
   int server_fd;
   int client_fd;
@@ -51,18 +111,24 @@ int main () {
       continue;
     }
   
-    printf("Подключился клиент: %s\n", inet_ntoa(client_addr.sin_addr)); // Для вывода айпи адреса клиента
+    printf("Подключился клиент: %s\n", inet_ntoa(client_addr.sin_addr)); 
     
     char buffer[1024] = {0};
     read(client_fd, buffer, sizeof(buffer) - 1);
     printf("Запрос:\n%s\n", buffer);
 
-    char responce[] = "HTTP/1.1 200 OK\r\n"
-                      "Content-Type: text/html\r\n"
-                      "Content-Length: 8\r\n"
-                      "\r\n"
-                      "Fuck you";
-    write(client_fd, responce, strlen(responce));
+    char method[16], path[256], protocol[16];
+    sscanf(buffer, "%s %s %s", method, path, protocol);
+    
+    char file_path[512];
+    if (strcmp(path, "/") == 0) {
+      snprintf(file_path, sizeof(file_path), "%s/index.html", WEB_DIR);
+    } else {
+      snprintf(file_path, sizeof(file_path), "%s%s", WEB_DIR, path);
+    }
+
+    send_file(client_fd, file_path);
+
     close(client_fd);
   }
 
